953e0068d462d9181e38085b29c3529d56a22eba
ziontee113 <ziontee113@gmail.com>
2022-11-04 09:15:38 +0700

    Add evdev examples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+mod _pick_device;
+
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+    evtest()
+}
+
+fn evtest() {
+    let mut d = _pick_device::pick_device();
+    println!("{}", d);
+    println!("Events:");
+    loop {
+        for ev in d.fetch_events().unwrap() {
+            println!("{:?}", ev);
+        }
+    }
 }

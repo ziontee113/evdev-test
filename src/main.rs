14b679c6c64915bb4861443a1698a1a78569fba7
ziontee113 <ziontee113@gmail.com>
2022-11-04 11:48:24 +0700

    Test emit KEY_A when pressing KEY_J on R1 Keyboard

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 #![allow(dead_code)]
 
-use evdev::{Device, InputEventKind, Key, MiscType, Synchronization};
+use std::{thread::sleep, time::Duration};
+
+use evdev::{
+    uinput::VirtualDeviceBuilder, AttributeSet, Device, EventType, InputEvent, InputEventKind, Key,
+    MiscType, Synchronization,
+};
 
 mod _pick_device;
 
@@ -13,24 +18,66 @@ fn main() {
         "usb-0000:00:1d.0-1.5.1.1/input0",
         "usb-0000:00:1d.0-1.5.3/input0",
     ];
-    test_hard_coded_device(device_paths.get(0).unwrap())
+    test_hard_coded_device(device_paths.get(1).unwrap())
+}
+
+fn virtual_input_test() -> std::io::Result<()> {
+    let mut keys = AttributeSet::<Key>::new();
+    keys.insert(Key::KEY_A);
+
+    let mut device = VirtualDeviceBuilder::new()?
+        .name("Fake Keyboard")
+        .with_keys(&keys)?
+        .build()
+        .unwrap();
+
+    for path in device.enumerate_dev_nodes_blocking()? {
+        let path = path?;
+        println!("Available as {}", path.display());
+    }
+
+    let type_ = EventType::KEY;
+    let code = Key::KEY_A.code();
+
+    println!("Waiting for Ctrl-C...");
+    loop {
+        let down_event = InputEvent::new(type_, code, 1);
+        device.emit(&[down_event]).unwrap();
+        println!("Pressed.");
+        sleep(Duration::from_secs(2));
+
+        let up_event = InputEvent::new(type_, code, 0);
+        device.emit(&[up_event]).unwrap();
+        println!("Released.");
+        sleep(Duration::from_secs(2));
+    }
 }
 
 fn test_hard_coded_device(name: &str) {
     let mut device = pick_device_from_path(name);
     device.grab().unwrap();
 
+    //
+    let mut keys = AttributeSet::<Key>::new();
+    keys.insert(Key::KEY_A);
+
+    let mut virtual_device = VirtualDeviceBuilder::new()
+        .unwrap()
+        .name("Fake Keyboard")
+        .with_keys(&keys)
+        .unwrap()
+        .build()
+        .unwrap();
+
+    let type_ = EventType::KEY;
+    let code = Key::KEY_A.code();
+
     loop {
         for ev in device.fetch_events().unwrap() {
             let kind = ev.kind();
-            if kind != InputEventKind::Synchronization(Synchronization(0))
-                && kind != InputEventKind::Misc(MiscType::MSC_SCAN)
-            {
-                if kind == InputEventKind::Key(Key::KEY_J) && ev.value() > 0 {
-                    println!("hello fucking venus")
-                } else {
-                    println!("ev {:#?}", ev)
-                }
+            if kind == InputEventKind::Key(Key::KEY_J) {
+                let le_event = InputEvent::new(type_, code, ev.value());
+                virtual_device.emit(&[le_event]).unwrap();
             }
         }
     }
